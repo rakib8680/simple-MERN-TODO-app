59de2a815c0a707c6ed2e48e94c83b11596bf3c1
rakib8680 <rakib.cst.1st@gmail.com>
2023-09-28 12:07:39 +0600

    add task ui created

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,27 +1,37 @@
-import React from 'react';
-import { useForm } from "react-hook-form"
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const ToDoForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm();
+  const onSubmit = (data) => console.log(data);
 
-    const onSubmit = (data) => console.log(data)
-
-
-    return (
-        <form onSubmit={handleSubmit(onSubmit)} className='flex bg-teal-600 py-8 px-5'>
-
-            <div className='flex'>
-                <input placeholder="what is the task today ?" type='text' {...register("task")} className='bg-teal-600  border-2 border-teal-900 ' />
-                <input type="submit" className=' bg-teal-900' />
-            </div>
-        </form>
-    );
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex bg-teal-600 py-12 px-8 rounded-md"
+    >
+      <div className="flex">
+        <input
+          placeholder="what is the task today ?"
+          type="text"
+          {...register("task")}
+          className="bg-teal-600 outline-none border-2 border-teal-900 placeholder:text-teal-900 ps-5 pr-20 py-1 text-teal-950 selection:bg-teal-400"
+        />
+        <button
+          type="submit"
+          className=" btn btn-sm rounded-none border-0 bg-teal-900 cursor-pointer h-9 hover:bg-teal-800 text-teal-600 px-3 "
+        >
+          Add Task
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default ToDoForm;
